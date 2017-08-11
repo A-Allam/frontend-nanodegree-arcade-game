8ce951d7e7bb62b488722e402b007adba7bf1195
aliallam <me.aliallam@gmail.com>
2017-08-11 16:40:59 +0200

    adding code to project

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
+// I made this project with some help from
+// https://github.com/mashablair/arcade-game/tree/master/js
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+
+    // setting initial location, speed
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random()*100) +100);
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -14,6 +22,18 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+    this.offset = 505;
+    this.start = -100;
+
+    if(this.x <= this.offset) {
+      this.x = this.x + this.speed * dt;
+    } else {
+      this.x = this.start;
+    }
+
+    // Handles collision with the Player (you need to implement)
+    // this.handlesCollision();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +44,105 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.x = 200;
+    this.y = 400;
+    this.sprite = 'char-pink-girl.png';
+};
 
+
+Player.prototype.update = function(dt) {
+    var self = this;
+    //if left key is pressed:
+    if(this.pressedKey === 'left' && this.x > 0) { //player isn't on left edge
+        this.x = this.x - 100;
+      console.log("left")
+    }
+
+    //if right key is pressed:
+    if(this.pressedKey === 'right' && this.x < 400) { //player isn't on right edge
+        this.x = this.x + 100;
+      console.log("right")
+
+    }
+
+    //if up key is pressed:
+    if(this.pressedKey === 'up' && this.y > 0) {
+        this.y = this.y - 90;
+      console.log("up")
+
+    }
+
+    //if down key is pressed:
+    if(this.pressedKey === 'down' && this.y < 400) {
+        this.y = this.y + 90;
+      console.log("down")
+
+    }
+
+    //this will make player jump only once when key is pressed:
+    this.pressedKey = null;
+
+    //if player reaches water, position reset:
+    if(this.y < 0) {
+        this.reset();
+    }
+
+    allEnemies.forEach(function(enemy) {
+    if(self.x >= enemy.x - 25 && self.x <= enemy.x + 25) {
+        if(self.y >= enemy.y - 25 && self.y <= enemy.y + 25) {
+            self.reset();
+            }
+        }
+    });
+};
+
+Player.prototype.render = function() {
+    console.log("this.sprite");
+    console.log(this.sprite);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// Player.prototype.render = function() {
+//     var playerImage = new Image();
+//     playerImage.src = this.sprite;
+
+//     playerImage.onload = function(){
+//       ctx.drawImage(playerImage, this.x, this.y);
+//     };
+
+//     // alert("playerImage", playerImage);
+// };
+
+//handleInput() method for player:
+Player.prototype.handleInput = function(e) {
+    this.pressedKey = e;
+};
+
+//Reset player to beginning position
+Player.prototype.reset = function() {
+    'use strict';
+   this.x = 200;
+   this.y = 400;
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var allEnemies = [];
 
+
+// Place the player object in a variable called player
+var player = new Player();
+
+
+// Instantiation of enemies and player objects:
+ //creates an array of Enemies
+(function displayEnemies() {
+    allEnemies.push(new Enemy(0, 50));
+    allEnemies.push(new Enemy(0, 140));
+    allEnemies.push(new Enemy(0, 230));
+}());
 
 
 // This listens for key presses and sends the keys to your
